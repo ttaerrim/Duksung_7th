f3ab4a8276f8660e15fd694e6c4a1589427c2a21
emiyamulzumdao <saligo96@gmail.com>
2019-05-21 22:58:18 +0900

    tip방 다운로드/테이블/페이지네이션 추가

--- tip/models.py
+++ tip/models.py
@@ -5,7 +5,7 @@ class Tip(models.Model):
     title=models.CharField(max_length=200)
     body=models.TextField()
     update_date=models.DateTimeField(auto_now=True)
-    file=models.FileField(null=True)
+    file=models.FileField(upload_to='tip/',null=True)
     hit=models.PositiveIntegerField(default=0)
     def __str__(self):
         return self.title
--- tip/urls.py
+++ tip/urls.py
@@ -6,10 +6,14 @@ from django.conf.urls.static import static
 urlpatterns=[
     path('post/',views.post,name='post'),
     path('detail/',views.detail,name='detail'),
-    path('show/',views.show,name='show'),
+    path('post_list/',views.post_list,name='post_list'),
     path('<int:tip_id>/',views.detail,name='detail'),
     path('<int:pk>/edit/',views.edit, name='edit'),
     path('<int:pk>/delete/',views.delete,name='delete'),
     path('deleteall/',views.deleteall,name='deleteall'),
     path('download/<int:pk>',views.download,name='download'),
+    #path('post_list/',views.post_list,name='post_list'),
+    #path('show/',views.show,name='show'),
+    
+    
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
