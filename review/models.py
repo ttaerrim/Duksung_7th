915f7fc0f23d5f73d957fd5295dfc46f4ac1c884
YoonaKim25 <oliviakim0205@gmail.com>
2019-05-24 11:28:09 +0900

    김윤아 - 대략적인 것 작성함.

--- review/models.py
+++ review/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    who = models.CharField(max_length=200)
+    #라디오 버튼 처리?? category
+    role = models.CharField(max_length=200)
+    write = models.TextField()
+
+    def __str__(self):
+        return self.who
