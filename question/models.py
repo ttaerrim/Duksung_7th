d0a8a28c06ad545fc12abdbbf39b03249285903e
jung4476700 <jung4476700@gmail.com>
2019-05-24 19:58:47 +0900

    crud

--- question/models.py
+++ question/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __Str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:50]
